d7bcb96d7d8e51ec0f82cea315c0e7c4904d2fd9
Mac <mac@Macs-MacBook-Pro.local>
2025-11-07 01:26:53 +0100

    client

--- worldbank/settings.py
+++ worldbank/settings.py
@@ -184,16 +184,11 @@ AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
 ]
 
-import os
-
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
+EMAIL_HOST_USER = 'worldbank2120@gmail.com'
+EMAIL_HOST_PASSWORD = 'kpewgpxtuxljiham'
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
-
-EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
-
-DEFAULT_FROM_EMAIL = f"World Bank <{EMAIL_HOST_USER}>"
-EMAIL_TIMEOUT = 20
+DEFAULT_FROM_EMAIL = 'World Bank <worldbank2120@gmail.com>'
